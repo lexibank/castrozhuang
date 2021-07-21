9783dc5420ac043301f9a75b129ddc12fb102934
chrzyki <christoph@functor-argument.net>
2021-07-21 11:06:12 +0200

    Release prep

--- lexibank_castrozhuang.py
+++ lexibank_castrozhuang.py
@@ -1,69 +1,57 @@
-from __future__ import unicode_literals, print_function
-from collections import OrderedDict, defaultdict
+from pathlib import Path
 
 import attr
-from pathlib import Path
-from clldutils.misc import slug
+import lingpy
+import pylexibank
 
-from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank import Concept, Language, FormSpec
-
-from lingpy import *
-from tqdm import tqdm
 
 @attr.s
-class HConcept(Concept):
+class HConcept(pylexibank.Concept):
     Chinese_Gloss = attr.ib(default=None)
 
+
 @attr.s
-class HLanguage(Language):
+class HLanguage(pylexibank.Language):
     Latitude = attr.ib(default=None)
     Longitude = attr.ib(default=None)
     ChineseName = attr.ib(default=None)
-    Family = attr.ib(default='Tai-Kadai')
-    SubGroup = attr.ib(default='Zhuang')
-    ISO = attr.ib(default=None)
+    Family = attr.ib(default="Tai-Kadai")
+    SubGroup = attr.ib(default="Zhuang")
     Location = attr.ib(default=None)
 
 
-class Dataset(BaseDataset):
-    id = 'castrozhuang'
+class Dataset(pylexibank.Dataset):
+    id = "castrozhuang"
     dir = Path(__file__).parent
     concept_class = HConcept
     language_class = HLanguage
-    
+
     def cmd_makecldf(self, args):
 
         args.writer.add_sources()
-        wl = Wordlist(self.dir.joinpath('raw', 'wordlist.tsv').as_posix())
-        langs = {} # need for checking later
+        wl = lingpy.Wordlist(self.dir.joinpath("raw", "wordlist.tsv").as_posix())
         concepts = {}
-
-        strip_concept = lambda x: x.replace(' ', '').replace('*', '')
+        strip_concept = lambda x: x.replace(" ", "").replace("*", "")
 
         for concept in self.conceptlists[0].concepts.values():
             args.writer.add_concept(
-                    ID=concept.id,
-                    Name=concept.english,
-                    Chinese_Gloss=strip_concept(concept.attributes['chinese']),
-                    Concepticon_ID=concept.concepticon_id,
-                    Concepticon_Gloss=concept.concepticon_gloss
-                    )
-            concepts[strip_concept(concept.attributes['chinese'])] = concept.id
-        langs = {k['ChineseName']: k['ID'] for k in self.languages}
+                ID=concept.id,
+                Name=concept.english,
+                Chinese_Gloss=strip_concept(concept.attributes["chinese"]),
+                Concepticon_ID=concept.concepticon_id,
+                Concepticon_Gloss=concept.concepticon_gloss,
+            )
+            concepts[strip_concept(concept.attributes["chinese"])] = concept.id
+        langs = {k["ChineseName"]: k["ID"] for k in self.languages}
         args.writer.add_languages()
 
-        bads = []
-        for idx in tqdm(wl, desc='cldfify'):
+        for idx in pylexibank.progressbar(wl, desc="cldfify"):
 
             args.writer.add_form_with_segments(
-               Language_ID=langs[wl[idx, 'doculect']],
-               Parameter_ID=concepts[strip_concept(wl[idx, 'concept'])],     
-               Value=wl[idx, 'value'],
-               Form=wl[idx, 'form'],
-               Segments=wl[idx, 'tokens'],
-               Source=['Castro2010a']
-               )
-
-
-
+                Language_ID=langs[wl[idx, "doculect"]],
+                Parameter_ID=concepts[strip_concept(wl[idx, "concept"])],
+                Value=wl[idx, "value"],
+                Form=wl[idx, "form"],
+                Segments=wl[idx, "tokens"],
+                Source=["Castro2010a"],
+            )
